deff1983c499dfc0bbdc66e14187d16a5be77d9d
sachin kumar <155023931+justsachin-kumar@users.noreply.github.com>
2024-01-04 00:09:41 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-def main():
-    name=input("what is your name?").strip().capitalize()
-    hello(name)
-    
-def hello(to):
-    print("hello,",to)
-        
-main()
-                                            
